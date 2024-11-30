70aefff1950fd3f222b834cda95673a124a7f1ec
Priiyanshu Singh <singhpriyanshu144@gmail.com>
2024-12-01 00:41:07 +0530

    remove about

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,4 @@
-import {About, Service, Contact, HeroSection} from "./index"
+import { Service, Contact, HeroSection} from "./index"
 const Home = () => {
   const imageUrl="https://raw.githubusercontent.com/thapatechnical/reactmultipage/b27bff8403d3729dcd652cff79d85c878a3f211a/public/images/hero.svg"
   return (
